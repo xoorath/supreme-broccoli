017b2e18a52b08bff0bfb8af09a832e693155059
xoorath <jared@xoorath.com>
2017-04-20 11:43:58 -0400

    Some OpenGL dependancies.

---
+++ Builds/Shared/CopyShared.bat
@@ -0,0 +1,7 @@
+
+echo f | xcopy /y /f "./Engine.ini" "../Win32_Debug/"
+echo f | xcopy /y /f "./Engine.ini" "../Win32_Release/"
+echo f | xcopy /y /f "./Engine.ini" "../Win32_Final/"
+echo f | xcopy /y /f "./Engine.ini" "../Win64_Debug/"
+echo f | xcopy /y /f "./Engine.ini" "../Win64_Release/"
+echo f | xcopy /y /f "./Engine.ini" "../Win64_Final/"
